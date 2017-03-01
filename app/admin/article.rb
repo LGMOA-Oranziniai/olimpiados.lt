772da0f26dbd7008e883cc91bbf43ef1efb8dc46
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2017-03-01 22:13:16 +0200

    Cleaner article admin panel

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -9,10 +9,18 @@ ActiveAdmin.register Article do
       link_to article.title, admin_article_path(article)
     end
     column :created_at
-    column "Category" do |article|
-      link_to article.category.name, admin_category_articles_path(article.category) if article.category
-    end
+    # column "Category" do |article|
+    #   link_to article.category.name, admin_category_a_path(article.category) if article.category
+    # end
     actions
+  end
+
+  show do |a|
+    div class: "content" do
+      h5 a.id
+      h2 a.title
+      article markdown(a.content)
+    end
   end
 
 end
