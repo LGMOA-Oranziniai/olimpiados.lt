29142b2200ccf8761bad15813af5b65e60b53ed3
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2017-03-04 14:45:56 +0200

    Fixed locale issue, pagination styling and title image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :set_site_locale
+  
+  def set_site_locale
+    I18n.locale = :lt
+  end
 
   def set_admin_locale
     I18n.locale = :en
