d21d6689c0aa57ed4f0e4148ddf8fef4e7f97a8b
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2017-03-12 13:11:26 +0200

    category now should be selectable

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -41,7 +41,7 @@ ActiveAdmin.register Article do
     f.inputs do
       input :title
       input :slug
-      input :category_id
+      input :category
       input :content
     end
     f.actions
